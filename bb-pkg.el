4bde349ad79cbbf6c81952322064f8d5de6ce5f8
Andrea Crotti <andrea.crotti.0@gmail.com>
2021-10-13 21:26:54 +0100

    commit

--- bb-pkg.el
+++ bb-pkg.el
@@ -1,6 +1,6 @@
 (define-package "bb-nrepl" "20210910.1959" "babashka + cider helpers"
   '()
-  :commit "f50b4c16ca2a73fd04ebd301f0bf2f5ab6107d88" :authors
+  :commit "838b0e2a04dd24e55a90c9e04449cb49af057643" :authors
   '(("Andrea Crotti" . "andrea.crotti.0@gmail.com"))
   :maintainer
   '("Andrea Crotti" . "andrea.crotti.0@gmail.com")
